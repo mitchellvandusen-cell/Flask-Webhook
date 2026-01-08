b5e62313ecf59c0b854b29804b02a0ecb56f8f59
mitchellvandusen-cell <mitchell_vandusen@hotmail.com>
2026-01-08 00:18:52 -0600

    updates

--- main.py
+++ main.py
@@ -518,7 +518,7 @@ def home():
 def stripe_webhook():
     payload = request.data
     sig_header = request.headers.get("Stripe-Signature")
-    endpoint_secret = os.getenv("STRIPE_WEBHOOK_SECRET")
+    endpoint_secret = os.getenv("ENDPOINT_SECRET")
 
     try:
         event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
